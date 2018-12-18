be846a63acb65700f0181296aac1979014924a5a
mael <mael.bournigal@ynov.com>
2018-12-18 12:29:02 +0100

    ajout scroll infini

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,10 +10,10 @@ import {DetailsPage} from "../details/details";
   templateUrl: 'home.html'
 })
 export class HomePage {
-  listMovies: Observable<any>;
-
+  listMovies = [];
+  page = 1;
   constructor(public navCtrl: NavController, public http: HttpClient, public movieApiProvider: MovieApiProvider) {
-    this.movieApiProvider.getMovies(1).subscribe((data => {
+    this.movieApiProvider.getMovies(this.page).subscribe((data => {
       this.listMovies = data.results;
       console.log(this.listMovies);
     }));
@@ -25,6 +25,22 @@ export class HomePage {
 
   detailsMovie(movie){
     this.navCtrl.push(DetailsPage, {movie: movie})
+  }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+    this.page++;
+    setTimeout(() => {
+      this.movieApiProvider.getMovies(this.page).subscribe((data => {
+        let newlist = data.results;
+        newlist.forEach((movie)=>{
+          this.listMovies.push(movie);
+        });
+      }));
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
   }
 }
 
